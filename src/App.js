a8b8d0134c7cee30442fe475ac21aebb7a3110f2
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-10 00:07:59 +0100

    feat: form to add styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [font, setFont] = useState("Open Sans");
+  const [bgColor, setBgColor] = useState("white");
+  const [fontSize, setFontSize] = useState(12);
+
+  const handleSubmit = event => {
+    event.preventDefault(); // Stop page refresh
+    alert("Style: " + font + ", " + fontSize + ", " + bgColor);
+
+    setFont("");
+    setBgColor("");
+    setFontSize("");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, World! Our DRP group is working hard to make the web more accessible to anyone!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <select value={font} name="font" onChange={event => setFont(event.target.value)}>
+            <option value="Open Sans">Open Sans</option>
+            <option value="Comic Sans">Comic Sans</option>
+            <option value="Roboto">Roboto</option>
+          </select>
+          <input type="number" value={fontSize} name="font_size" onChange={event => setFontSize(event.target.value)} placeholder="font size" />
+          <input type="text" value={bgColor} name="bgColor" onChange={event => setBgColor(event.target.value)} />
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
