6fad476a1fa5d75512add47343860cde138ed376
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-10 00:23:16 +0100

    feat: list styles from db

--- src/App.js
+++ src/App.js
@@ -2,7 +2,40 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 
+class Style {
+  constructor(font, fontSize, bgColor) {
+    this.font = font;
+    this.fontSize = fontSize;
+    this.bgColor = bgColor;
+  }
+}
+
 function App() {
+  const testRows = [new Style("Open Sans", 20, "orange"), new Style("Arial", 2, "green")]
+  return (
+    <div className="App">
+      <header className="App-header">
+        {form()}
+        {styles(testRows)}
+      </header>
+    </div>
+  );
+}
+
+function styles(styles) {
+  const rows = [];
+  for (let i = 0; i < styles.length; i++) {
+    const style = styles[i];
+    rows.push(
+      <tr>
+        <td>{style.font + ", " + style.fontSize + ", " + style.bgColor}</td>
+      </tr>
+    )
+  }
+  return rows;
+}
+
+function form() {
   const [font, setFont] = useState("Open Sans");
   const [bgColor, setBgColor] = useState("white");
   const [fontSize, setFontSize] = useState(12);
@@ -12,25 +45,21 @@ function App() {
     alert("Style: " + font + ", " + fontSize + ", " + bgColor);
 
     setFont("");
-    setBgColor("");
     setFontSize("");
+    setBgColor("");
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <select value={font} name="font" onChange={event => setFont(event.target.value)}>
-            <option value="Open Sans">Open Sans</option>
-            <option value="Comic Sans">Comic Sans</option>
-            <option value="Roboto">Roboto</option>
-          </select>
-          <input type="number" value={fontSize} name="font_size" onChange={event => setFontSize(event.target.value)} placeholder="font size" />
-          <input type="text" value={bgColor} name="bgColor" onChange={event => setBgColor(event.target.value)} />
-          <input type="submit" value="Submit" />
-        </form>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <select value={font} name="font" onChange={event => setFont(event.target.value)}>
+        <option value="Open Sans">Open Sans</option>
+        <option value="Comic Sans">Comic Sans</option>
+        <option value="Roboto">Roboto</option>
+      </select>
+      <input type="number" value={fontSize} name="font_size" onChange={event => setFontSize(event.target.value)} placeholder="font size" />
+      <input type="text" value={bgColor} name="bgColor" onChange={event => setBgColor(event.target.value)} />
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
