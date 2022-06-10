6e731616123eda3556e20493979f853eff3f6c06
Nicholas Fry <nf20@ic.ac.uk>
2022-06-10 17:18:01 +0100

    feat: make welcome text appropriate

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Puxa Saco
+          Welcome to Claraify!
         </p>
         <a
           className="App-link"
