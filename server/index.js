39e4c50d553902faeed6084322b666ea78edd310
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-11 03:05:25 +0100

    build: update build for heroku deployment 	- downgrade node version 	- use local port 4001 	- create client with redis endpoint

--- server/index.js
+++ server/index.js
@@ -4,43 +4,39 @@ const redis = require('redis');
 // ==== retrieve env variables ====
 require('dotenv').config();
 
-const sanitizeRedisUrl = url => url.replace(/^(redis\:\/\/)/, '');
-
 const { REDIS_ENDPOINT_URI, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 const endpointUri = REDIS_ENDPOINT_URI
-    ? sanitizeRedisUrl(REDIS_ENDPOINT_URI)
-    : `${sanitizeRedisUrl(REDIS_HOST)}:${REDIS_PORT}`;
+    ? REDIS_ENDPOINT_URI
+    : `${REDIS_HOST}:${REDIS_PORT}`;
 
-const password = REDIS_PASSWORD || undefined;
+const password = REDIS_PASSWORD || undefined
 
 // ==== end of retrieve env variables ====
 
-const PORT = 3001; // uncomment for deployment
+const PORT = process.env.PORT || 4001;
+const BUILD = '../client/build/'
 
 const app = express();
 
 app.use(express.json());
 
 const path = require('path')
+
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, BUILD)))
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
 app.get("/getall", async (req, res) => {
-    client = redis.createClient(); //endpointUri, password);
+    client = redis.createClient(`redis://${endpointUri}`, password);
     console.log("getall: checkpoint [1]");
     await client.connect();
     console.log("getall: checkpoint [2]");
-    // styles = await client.lrange('demo', 0, -1, (error, styles) => {
-        
-    // });
-    // styles.forEach(v => console.log(v));
-    // res.json({});
-    res.json( await client.get('demo', (error, styles) => {
+
+    res.json(await client.get('demo', (error, styles) => {
         if (error) console.error(error);
         if (styles != null) {
             return styles;
@@ -50,27 +46,20 @@ app.get("/getall", async (req, res) => {
 
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../client/build/index.html'))
+    res.sendFile(path.join(__dirname + BUILD + 'index.html'))
 })
 
 app.post("/set", async (req, res) => {
     const { data } = req.body;
-    client = redis.createClient();
+    client = redis.createClient(`redis://${endpointUri}`, password);
     console.log("set: checkpoint [1]");
     await client.connect();
     console.log("set: checkpoint [2]");
     // const {font, fontSize, bgColor} = req.body;
     console.log(data);
     client.set('demo', JSON.stringify(data));
-    // client.set('demo', "{\"font\":\"demoStyle\",\"fontSize\":12,\"bgColor\":\"red\"}");
-    // res.json({ message: await client.get('demo', (error, styles) => {
-    //     if (error) console.error(error);
-    //     if (styles != null) {
-    //         return JSON.parse(styles);
-    //     }
-    // })});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
