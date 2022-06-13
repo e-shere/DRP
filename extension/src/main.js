13e1b5b78eb089d1aa40dc86c8670baedf5faf31
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-13 12:18:13 +0100

    feat: blue bg color

--- extension/src/main.js
+++ extension/src/main.js
@@ -1,7 +1,7 @@
 /*global chrome*/
 
 export async function changeColor() {
-    chrome.storage.sync.set({color: "#3aa757"}, ()=>{});
+    chrome.storage.sync.set({color: "#c1e6dd"}, ()=>{});
   
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
       
