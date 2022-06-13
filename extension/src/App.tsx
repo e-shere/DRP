efdbd68e78d0a7f20b3178770f13d1a13b952389
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-06-13 11:42:55 +0100

    feat: Add font changing button

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { changeColor } from './main.js';
+import { changeColor, changeFont } from './main.js';
 
 function App() {
   return (
@@ -9,6 +9,9 @@ function App() {
       <header className="App-header">
       <button onClick={changeColor}>
           Background colour
+      </button>
+      <button onClick={changeFont}>
+          Font
       </button>
       </header>
     </div>
--- extension/src/main.js
+++ extension/src/main.js
@@ -16,4 +16,20 @@ function setPageBackgroundColor() {
     chrome.storage.sync.get("color", ({ color }) => {
       document.body.style.backgroundColor = color;
     });
-  }
+}
+
+export async function changeFont() {
+  chrome.storage.sync.set({font: "Arial"}, ()=>{});
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+      
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageFont,
+    });
+}
+
+function setPageFont() {
+  chrome.storage.sync.get("font", ({ font }) => {
+    document.body.style.fontFamily = font;
+  });
+}
