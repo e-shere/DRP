c767da080102b04b01deedf75650d8fba1988393
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-06-13 03:42:30 +0100

    feat: Add button to extension

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { changeColor } from './main.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Claraify!
-        </p>
-        <a
-          className="App-link"
-          href="http://claraify.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Info
-        </a>
+      <button onClick={changeColor}>
+          Background colour
+      </button>
       </header>
     </div>
   );
