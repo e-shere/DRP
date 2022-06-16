4e0e914e82cd9b05454ef3b5b9c07453360a591c
Nicholas Fry <nf20@ic.ac.uk>
2022-06-16 11:51:53 +0100

    feat: console log styles in table

--- client/src/preset-selection.tsx
+++ client/src/preset-selection.tsx
@@ -9,9 +9,11 @@ import Style from "./style";
 export default function DataTable(styles: Style[]) {
   const rows: TableRow[] = [];
   for (let i = 0; i < styles.length; i ++) {
+    console.log(`${styles[i]}  style number ${i}`);
     rows.push({id: i, style: styles[i]})
   }
-  let radioChecked: GridRowId[] = [rows[0].id];
+
+  var radioChecked: GridRowId[] = [rows[0].id];
   const [selectionModel, setSelectionModel] = React.useState<GridRowId[]>([]);
   radioChecked = selectionModel;
 
@@ -20,26 +22,26 @@ export default function DataTable(styles: Style[]) {
   });
 
 
-const columns = [
-  {
-    field: "radiobutton",
-    headerName: "",
-    width: 100,
-    sortable: false,
-    renderCell: (params: any) => (
-      <Radio checked={radioChecked[0] === params.id} value={params.id} />
-    )
-  },
-  {
-    field: "id",
-    headerName: "ID"
-  },
-  {
-    field: "style",
-    headerName: "Style",
-    width: 150
-  },
-];
+  const columns = [
+    {
+      field: "radiobutton",
+      headerName: "",
+      width: 100,
+      sortable: false,
+      renderCell: (params: any) => (
+        <Radio checked={radioChecked[0] === params.id} value={params.id} />
+      )
+    },
+    {
+      field: "id",
+      headerName: "ID"
+    },
+    {
+      field: "style",
+      headerName: "Style",
+      width: 150
+    },
+  ];
 
   return (
       <DataGrid
