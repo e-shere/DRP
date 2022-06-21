2eca381efe5b28876c7c87426864157f9609d2d3
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-21 19:47:03 +0100

    fix: full width font select, default colors

--- extension/src/Settings.tsx
+++ extension/src/Settings.tsx
@@ -31,13 +31,13 @@ function BackgroundSettings(settings: UserSettings, setSettings: (_: UserSetting
   return (
     <FormControl fullWidth>
       <div className="setting">
-          <SketchPicker
-            width="92.5%"
-            presetColors={PRESET_BG_COLORS}
-            color={settings.bgColor}
-            onChange={color => { setSettings({ ...settings, bgColor: color.hex }) }}
-          />
-        </div>
+        <SketchPicker
+          width="92.5%"
+          presetColors={PRESET_BG_COLORS}
+          color={settings.bgColor}
+          onChange={color => { setSettings({ ...settings, bgColor: color.hex }) }}
+        />
+      </div>
     </FormControl>
   );
 }
@@ -45,51 +45,52 @@ function BackgroundSettings(settings: UserSettings, setSettings: (_: UserSetting
 function FontSettings(settings: UserSettings, setSettings: (_: UserSettings) => void) {
   return (
     <FormControl fullWidth>
-        <div className="setting">
-          <TextField
-            select
-            label="Font"
-            value={settings.font}
-            onChange={event => { setSettings({ ...settings, font: event.target.value }) }}
-          >
-            <MenuItem value="Arial">Arial</MenuItem>
-            <MenuItem value="Courier New">Courier New</MenuItem>
-            <MenuItem value="Roboto">Roboto</MenuItem>
-          </TextField>
-        </div>
-        {NumberField(
-          "Font Size",
-          -5,
-          50,
-          settings.fontSize,
-          event => { setSettings({ ...settings, fontSize: Number(event.target.value) }) }
-        )}
-        {NumberField(
-          "Letter Spacing",
-          0,
-          20,
-          settings.letterSpacing,
-          event => { setSettings({ ...settings, letterSpacing: Number(event.target.value) }) }
-        )}
-        {NumberField(
-          "Line Spacing",
-          0,
-          30,
-          settings.lineSpacing,
-          event => { setSettings({ ...settings, lineSpacing: Number(event.target.value) }) }
-        )}
-        <div className="setting">
-          <label className="color-input-label">Font Colour</label>
-          <SketchPicker
-            width="92.5%"
-            presetColors={["#ffffff", "#000000"]}
-            color={settings.fontColor}
-            onChange={color => { setSettings({ ...settings, fontColor: color.hex }) }}
-          />
-        </div>
-      </FormControl>
+      <div className="setting">
+        <TextField
+          fullWidth
+          select
+          label="Font"
+          value={settings.font}
+          onChange={event => { setSettings({ ...settings, font: event.target.value }) }}
+        >
+          <MenuItem value="Arial">Arial</MenuItem>
+          <MenuItem value="Courier New">Courier New</MenuItem>
+          <MenuItem value="Roboto">Roboto</MenuItem>
+        </TextField>
+      </div>
+      {NumberField(
+        "Font Size",
+        -5,
+        50,
+        settings.fontSize,
+        event => { setSettings({ ...settings, fontSize: Number(event.target.value) }) }
+      )}
+      {NumberField(
+        "Letter Spacing",
+        0,
+        20,
+        settings.letterSpacing,
+        event => { setSettings({ ...settings, letterSpacing: Number(event.target.value) }) }
+      )}
+      {NumberField(
+        "Line Spacing",
+        0,
+        30,
+        settings.lineSpacing,
+        event => { setSettings({ ...settings, lineSpacing: Number(event.target.value) }) }
+      )}
+      <div className="setting">
+        <label className="color-input-label">Font Colour</label>
+        <SketchPicker
+          width="92.5%"
+          presetColors={["#ffffff", "#000000"]}
+          color={settings.fontColor}
+          onChange={color => { setSettings({ ...settings, fontColor: color.hex }) }}
+        />
+      </div>
+    </FormControl>
   );
 }
 
 
-export {BackgroundSettings, FontSettings};
+export { BackgroundSettings, FontSettings };
