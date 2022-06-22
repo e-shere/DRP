d674ef5ffe2692bf050a9a7bed53b4d5fc0e4990
Elizaveta Sheremetyeva <es120@ic.ac.uk>
2022-06-22 19:47:36 +0100

    feat: Change font colour to optimum (b/w) on bg colour change

--- extension/src/Main.tsx
+++ extension/src/Main.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, SyntheticEvent, useState } from "react";
 import { Switch } from "@mui/material";
-import { Accordion, AccordionSummary, AccordionDetails } from "./accordion";
+import { Accordion, AccordionSummary, AccordionDetails } from "./Accordion";
 import Typography from '@mui/material/Typography';
 
 import { TITLE, Preset, UserSettings } from "./App";
