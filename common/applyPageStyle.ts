bdf8c0b3e7c344c08ac659309808d8f2b91338ea
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2022-06-28 15:10:09 +0100

    refactor: check changed once for each toggle

--- common/applyPageStyle.ts
+++ common/applyPageStyle.ts
@@ -1,5 +1,6 @@
 import { UserSettings, Preset } from "./domain";
 
+/* Tags for elements to exclude should be uppercase */
 function applyPageStyle(s: UserSettings, p: Preset, getRootElement?: (_: Document) => HTMLElement) {
   const HEADINGS = ["H1", "H2", "H3", "H4", "H5", "H6"];
   const HEADING_SCALING = 1.5;
@@ -13,72 +14,71 @@ function applyPageStyle(s: UserSettings, p: Preset, getRootElement?: (_: Documen
   /* Reset page */
   rootElement.innerHTML = rootElement.dataset.initialHtml ?? "Error: Clarify could not load page";
 
-  /* Apply punctuation spacing */
   if (s.styleChanged) {
+    if (p.punctuationSpacingChanged) {
+      rootElement.querySelectorAll<HTMLElement>("P, LI, SPAN").forEach(element => {
+        setPunctuationSpacing(element);
+      });
+    }
+
     rootElement.querySelectorAll<HTMLElement>("*").forEach(element => {
-      /* Tags for elements to exclude should be uppercase */
-      setElementProperty(element, "background-color", p.bgColor, p.bgChanged,
-        includeTags([...HEADINGS, "A", "P", "HEADER", "LI", "BODY", "FRAMESET"])
-      );
-      setElementProperty(element, "font-family", p.font, p.fontChanged,
-        excludeTags(["IMG", "SPAN"])
-      );
-      setElementProperty(element, "color", p.fontColor, p.bgChanged,
-        excludeTags(["IMG", "A"])
-      );
-      setElementProperty(element, "color", p.auxFontColor, p.bgChanged,
-        includeTags(["A"])
-      );
+      if (p.bgChanged) {
+        document.body.style.backgroundColor = p.bgColor;
+        setElementProperty(element, "background-color", p.bgColor,
+          includeTags([...HEADINGS, "A", "P", "HEADER", "LI"])
+        );
+        setElementProperty(element, "color", p.fontColor, excludeTags(["IMG", "A"]));
+        setElementProperty(element, "color", p.auxFontColor, includeTags(["A"]));
+      }
 
-      setElementProperty(element, "font-size", `${p.fontSize}px`, p.fontChanged,
-        excludeTags([...HEADINGS, "IMG"])
-      );
-      setElementProperty(element, "font-size", `${p.fontSize * HEADING_SCALING}px`, p.fontChanged,
-        includeTags(HEADINGS)
-      );
-
-      setElementProperty(element, "line-height", `${p.lineSpacing * p.fontSize}px`, p.fontChanged,
-        excludeTags([...HEADINGS, "IMG"])
-      );
-      setElementProperty(element, "line-height", `${p.lineSpacing * p.fontSize * HEADING_SCALING}px`, p.fontChanged,
-        includeTags(HEADINGS)
-      );
-
-      setElementProperty(element, "letter-spacing", `${p.letterSpacing * p.fontSize}px`, p.fontChanged,
-        excludeTags([...HEADINGS, "IMG"])
-      );
-      setElementProperty(element, "letter-spacing", `${p.letterSpacing * p.fontSize * HEADING_SCALING}px`, p.fontChanged,
-        includeTags(HEADINGS)
-      );
-
-      setPunctuationSpacing(element, ["P", "LI", "SPAN"]);
+      if (p.fontChanged) {
+        const heading_size = p.fontSize * HEADING_SCALING;
+        setElementProperty(element, "font-family", p.font,
+          excludeTags(["IMG", "SPAN"])
+        );
+        setElementProperty(element, "font-size", `${p.fontSize}px`,
+          excludeTags([...HEADINGS, "IMG"])
+        );
+        setElementProperty(element, "font-size", `${heading_size}px`,
+          includeTags(HEADINGS)
+        );
+        setElementProperty(element, "line-height", `${p.lineSpacing * p.fontSize}px`,
+          excludeTags([...HEADINGS, "IMG"])
+        );
+        setElementProperty(element, "line-height", `${p.lineSpacing * heading_size}px`,
+          includeTags(HEADINGS)
+        );
+        setElementProperty(element, "letter-spacing", `${p.letterSpacing * p.fontSize}px`,
+          excludeTags([...HEADINGS, "IMG"])
+        );
+        setElementProperty(element, "letter-spacing", `${p.letterSpacing * heading_size}px`,
+          includeTags(HEADINGS)
+        );
+      }
     });
   }
 
-  function setPunctuationSpacing(element: HTMLElement, includedTags: string[]) {
-    if (p.punctuationSpacingChanged && includedTags.includes(element.tagName)) {
-      const doc = new DOMParser().parseFromString(element.innerHTML, 'text/html');
-      const arr = Array.from(doc.body.childNodes)
-        .map(child => (child as HTMLElement).outerHTML || (child.textContent ?? "")
-          .split(/(?<=[.?!,;])/).join(p.punctuationSpace));
-      element.innerHTML = arr.join('');
-    }
+  function setPunctuationSpacing(element: HTMLElement) {
+    const doc = new DOMParser().parseFromString(element.innerHTML, 'text/html');
+    const arr = Array.from(doc.body.childNodes)
+      .map(child => (child as HTMLElement).outerHTML || (child.textContent ?? "")
+        .split(/(?<=[.?!,;])/).join(p.punctuationSpace));
+    element.innerHTML = arr.join('');
   }
 
   function setElementProperty(
     element: HTMLElement,
     property: string,
     value: string,
-    changed: boolean,
     applyOnTag: (_: HTMLElement) => boolean
   ) {
-    if (applyOnTag(element) && changed) {
+    if (applyOnTag(element)) {
       element.style.setProperty(property, value);
     }
   }
 
   function excludeTags(tags: string[]) {
-    return ((element: HTMLElement) => !includeTags(tags)(element));
+    return (element: HTMLElement) => !includeTags(tags)(element);
   }
 
   function includeTags(tags: string[]) {
