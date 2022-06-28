cd83164acb86d74ca7ba791a477a09f4f3b86562
Filippo Selvatici <fs2220@imperial.ac.uk>
2022-06-28 02:18:55 +0100

    feat: silently prevent the extension to update the page of the webapp

--- extension/src/pageStyle.ts
+++ extension/src/pageStyle.ts
@@ -9,12 +9,16 @@ async function updatePage(settings: UserSettings, preset: Preset) {
 
 async function setPageStyle(settings: UserSettings, preset: Preset, applyPageStyle: (s: UserSettings, p: Preset, getRootElement?: (_: Document) => HTMLElement) => void) {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const demoAppUrls = ["https://clarify-this.herokuapp.com/", "https://clarify-this-staging.herokuapp.com/", "http://localhost:4001/"];
+  if (!demoAppUrls.includes(tab.url ?? "")) {
+    /* Update the page content if it is not the webapp */
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id ? tab.id : -1 },
+      func: applyPageStyle,
+      args: [settings, preset],
+    });
+  }
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id ? tab.id : -1 },
-    func: applyPageStyle,
-    args: [settings, preset],
-  });
 }
 
 export { updatePage };
